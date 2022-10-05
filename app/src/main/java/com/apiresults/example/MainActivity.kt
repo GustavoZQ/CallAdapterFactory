1193a6bcb1b7b7140fc561de2a0323ef1b34adbe
Gdx741 <zacarias_741@hotmail.es>
2022-10-04 22:10:44 -0500

    first commit

--- app/src/main/java/com/apiresults/example/MainActivity.kt
+++ app/src/main/java/com/apiresults/example/MainActivity.kt
@@ -2,13 +2,10 @@ package com.apiresults.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.apiresults.bygdx.ApiCallAdapterFactory
-import com.apiresults.bygdx.ApiResult
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
